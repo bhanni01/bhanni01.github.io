4e9a6b317b90e00cac81d1cbe8fe36c4a21522f2
bhanni01 <114847745+bhanni01@users.noreply.github.com>
2025-12-05 12:32:44 -0600

    Deploy React personal site with star background

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+function App() {
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '50px' }}>
+      <h1>Hi, I’m Nischal!</h1>
+      <p>Welcome to my personal website.</p>
+      <p>
+        Check out my projects on <a href="https://github.com/bhanni01" style={{ color: 'lightblue' }}>GitHub</a>.
+      </p>
+    </div>
+  );
+}
+
+export default App;
+
